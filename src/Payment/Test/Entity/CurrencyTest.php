937ecce019b32a0fd2ee71b65a15b19bdb59e8ee
Flaaim <flaaim@list.ru>
2025-10-08 09:27:37 +1000

    added Currency

---
+++ src/Payment/Test/Entity/CurrencyTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Payment\Test\Entity;
+
+use App\Payment\Entity\Currency;
+use PHPUnit\Framework\TestCase;
+
+class CurrencyTest extends TestCase
+{
+    public function testSuccess(): void
+    {
+        $currency = new Currency('RUB');
+        $this->assertEquals('RUB', $currency->getValue());
+    }
+
+    public function testInvalid(): void
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        new Currency('RKZ');
+    }
+
+    public function testEmpty(): void
+    {
+        $currency = new Currency();
+        $this->assertEquals('RUB', $currency->getValue());
+    }
+
+}
