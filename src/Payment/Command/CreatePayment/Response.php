581e4be5091f333c0e6ef9b1298c26f7ae21f70b
Flaaim <flaaim@list.ru>
2025-10-10 16:59:35 +1000

    added JsonSerialize

--- src/Payment/Command/CreatePayment/Response.php
+++ src/Payment/Command/CreatePayment/Response.php
@@ -2,11 +2,22 @@
 
 namespace App\Payment\Command\CreatePayment;
 
-readonly class Response
+class Response implements \JsonSerializable
 {
     public function __construct(
         public float  $amount,
         public string $currency,
         public string $status,
+        public string $returnUrl,
     ){}
+
+    public function jsonSerialize(): array
+    {
+        return [
+            'amount' => $this->amount,
+            'currency' => $this->currency,
+            'status' => $this->status,
+            'returnUrl' => $this->returnUrl,
+        ];
+    }
 }
