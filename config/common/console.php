bd9ff0511fa9bb7d87b97900aa986e06fbf7aacd
Flaaim <flaaim@list.ru>
2025-10-10 11:09:17 +1000

    added Fixture Load Command

--- config/common/console.php
+++ config/common/console.php
@@ -2,18 +2,33 @@
 
 declare(strict_types=1);
 
+use App\Command\FixturesLoadCommand;
 use Doctrine\Migrations\Tools\Console\Command\DiffCommand;
 use Doctrine\Migrations\Tools\Console\Command\ExecuteCommand;
 use Doctrine\Migrations\Tools\Console\Command\LatestCommand;
 use Doctrine\Migrations\Tools\Console\Command\MigrateCommand;
 use Doctrine\Migrations\Tools\Console\Command\StatusCommand;
 use Doctrine\Migrations\Tools\Console\Command\UpToDateCommand;
+use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\ORM\Tools\Console\Command\ValidateSchemaCommand;
+use Psr\Container\ContainerInterface;
 
 return [
+    FixturesLoadCommand::class => static function (ContainerInterface $container) {
+        $config = $container->get('config')['console'];
+
+        /** @var EntityManagerInterface $em  $em */
+        $em = $container->get(EntityManagerInterface::class);
+
+        return new FixturesLoadCommand(
+            $em,
+            $config['fixture_paths']
+        );
+    },
     'config' => [
         'console' => [
             'commands' => [
+                FixturesLoadCommand::class,
                 ValidateSchemaCommand::class,
 
                 DiffCommand::class,
@@ -22,7 +37,10 @@ return [
                 LatestCommand::class,
                 StatusCommand::class,
                 UpToDateCommand::class,
+            ],
+            'fixture_paths' => [
+                __DIR__ . '/../../src/Product/Fixture'
             ]
-        ]
+        ],
     ]
 ];
