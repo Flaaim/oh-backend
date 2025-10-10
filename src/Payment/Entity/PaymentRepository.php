216002b475d026cbbeaf482a7a95d91b77977b39
Flaaim <flaaim@list.ru>
2025-10-10 16:59:18 +1000

    implement Repositories

--- src/Payment/Entity/PaymentRepository.php
+++ src/Payment/Entity/PaymentRepository.php
@@ -2,11 +2,23 @@
 
 namespace App\Payment\Entity;
 
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\EntityRepository;
+
 class PaymentRepository
 {
-    public function create(Payment $payment)
+    private EntityRepository $repo;
+    private EntityManagerInterface $em;
+    public function __construct(EntityManagerInterface $em)
     {
+        $repo = $em->getRepository(Payment::class);
+        $this->repo = $repo;
+        $this->em = $em;
 
+    }
+    public function create(Payment $payment): void
+    {
+        $this->em->persist($payment);
     }
 
 }
--- src/Product/Entity/ProductRepository.php
+++ src/Product/Entity/ProductRepository.php
@@ -3,11 +3,26 @@
 namespace App\Product\Entity;
 
 use App\Shared\ValueObject\Id;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\EntityRepository;
 
 class ProductRepository
 {
-    public function findById(Id $id): Product
+    private EntityRepository $repo;
+    private EntityManagerInterface $em;
+    public function __construct(EntityManagerInterface $em)
     {
+        $repo = $em->getRepository(Product::class);
+        $this->repo = $repo;
+        $this->em = $em;
+    }
 
+    public function get(Id $id): Product
+    {
+        if(!$product = $this->repo->find($id)) {
+            throw new \DomainException('Product not found.');
+        }
+        /** @var Product $product */
+        return $product;
     }
 }
