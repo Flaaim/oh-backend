4282fbbd8aef6a73fd84391a16bc59339fc66c7e
Flaaim <flaaim@list.ru>
2025-10-12 14:42:26 +1000

    updated YookassaWebhookParser

--- src/Shared/Domain/Service/Payment/WebhookParser/YookassaWebhookParser.php
+++ src/Shared/Domain/Service/Payment/WebhookParser/YookassaWebhookParser.php
@@ -10,40 +10,37 @@ use InvalidArgumentException;
 class YookassaWebhookParser implements PaymentWebhookParserInterface
 {
     const PROVIDER_NAME = 'Yookassa';
-    const ALLOWED_DATA = [
-        'status',
-        'paymentId',
-        'amount',
-        'currency',
-        'metadata',
-    ];
+    const NOTIFICATION_TYPE = 'notification';
+
     public function supports(string $provider, array $data): bool
     {
         if ($provider !== self::PROVIDER_NAME) {
             return false;
         }
-        foreach (self::ALLOWED_DATA as $requiredData) {
-            if(!array_key_exists($requiredData, $data)) {
-                return false;
-            }
-        }
-        return true;
+
+        return isset($data['object']['id'])
+            && isset($data['type'])
+            && $data['type'] === self::NOTIFICATION_TYPE;
+
     }
 
     public function parse(array $data): PaymentWebhookDataInterface
     {
-        foreach (self::ALLOWED_DATA as $requiredField) {
-            if (!array_key_exists($requiredField, $data)) {
-                throw new InvalidArgumentException("Missing required field: {$requiredField}");
-            }
+        if (!$this->supports(self::PROVIDER_NAME, $data)) {
+            throw new InvalidArgumentException("Invalid Yookassa webhook format");
         }
 
+        $object = $data['object'];
+
+        $amount = $object['amount']['value'] ?? '0';
+        $currency = $object['amount']['currency'] ?? 'RUB';
+
         return new YookassaWebhookData(
-            $data['status'],
-            $data['amount'],
-            $data['metadata'],
-            $data['paymentId'],
-            $data['currency']
+            $object['status'],
+            $amount,
+            $object['metadata'] ?? [],
+            $object['id'],
+            $currency
         );
     }
 }
