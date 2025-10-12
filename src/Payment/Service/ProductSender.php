10d810ecdc516803cc7c03fc93dd617f36b47cb0
Flaaim <flaaim@list.ru>
2025-10-12 10:20:19 +1000

    Added product to productsender

--- src/Payment/Service/ProductSender.php
+++ src/Payment/Service/ProductSender.php
@@ -3,10 +3,12 @@
 namespace App\Payment\Service;
 
 use App\Payment\Entity\Email;
+use App\Product\Entity\Product;
 use Symfony\Component\Mailer\Exception\TransportException;
 use Symfony\Component\Mailer\Exception\TransportExceptionInterface;
 use Symfony\Component\Mailer\MailerInterface;
-
+use Symfony\Component\Mime\Part\DataPart;
+use Symfony\Component\Mime\Part\File;
 
 
 class ProductSender
@@ -16,18 +18,25 @@ class ProductSender
     {
         $this->mailer = $mailer;
     }
-    public function send(Email $email): void
+    public function send(Email $email, Product $product): void
     {
         $message = new \Symfony\Component\Mime\Email();
-        $message->subject('Образец документов');
+        $message->subject($product->getName());
         $message->to($email->getValue());
-        $message->text(
-            'some text'
+        $message->html(
+            'Спасибо за покупку! Образцы документов мы приложили к этому письму'
+        );
+        $message->addPart(
+            new DataPart(
+                new File(
+                    $product->getPath(),
+                )
+            )
         );
         try{
             $this->mailer->send($message);
-        } catch (TransportExceptionInterface $exception) {
-            throw new TransportException($exception->getMessage());
+        } catch (TransportExceptionInterface $e) {
+            throw new TransportException($e->getMessage());
         }
 
     }
--- src/Payment/Test/Service/ProductSenderTest.php
+++ src/Payment/Test/Service/ProductSenderTest.php
@@ -3,22 +3,32 @@
 namespace App\Payment\Test\Service;
 
 use App\Payment\Service\ProductSender;
+use App\Product\Entity\Currency;
+use App\Product\Entity\Price;
+use App\Product\Entity\Product;
+use App\Shared\ValueObject\Id;
 use PHPUnit\Framework\TestCase;
 use Symfony\Component\Mailer\Exception\TransportException;
 use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Mime\Address;
 use Symfony\Component\Mime\Email;
 use App\Payment\Entity\Email as UserEmail;
+use Symfony\Component\Mime\Part\DataPart;
+use Symfony\Component\Mime\Part\File;
 
 class ProductSenderTest extends TestCase
 {
     public function testSuccess()
     {
+        $product = $this->getProduct();
         $email = new UserEmail('test@app.ru');
-        $text = 'some text';
-        $subject = 'Образец документов';
+        $text = 'Спасибо за покупку! Образцы документов мы приложили к этому письму';
+        $subject = $product->getName();
 
-        $message = (new Email())->subject($subject)->to($email->getValue())->text($text);
+
+        $message = (new Email())->subject($subject)->to($email->getValue())->html($text)->addPart(
+            new DataPart(new File($product->getPath()))
+        );
 
         $mailer = $this->createMock(MailerInterface::class);
         $mailer->expects($this->once())->method('send')->with(
@@ -27,21 +37,37 @@ class ProductSenderTest extends TestCase
             /** @var Email $message */
             self::assertEquals([new Address($email->getValue())], $message->getTo());
             self::assertEquals($subject, $message->getSubject());
-            self::assertEquals($text, $message->getTextBody());
+            self::assertEquals($text, $message->getHtmlBody());
+
         });
 
         $productSender = new ProductSender($mailer);
-        $productSender->send($email);
+        $productSender->send($email, $product);
     }
 
     public function testFailed(): void
     {
+        $product = $this->getProduct();
         $email = new UserEmail('test@app.ru');
         $mailer = $this->createMock(MailerInterface::class);
         $mailer->expects($this->once())->method('send')->willThrowException(new TransportException());
 
         $productSender = new ProductSender($mailer);
+
         $this->expectException(TransportException::class);
-        $productSender->send($email);
+        $productSender->send($email, $product);
+    }
+
+    private function getProduct(): Product
+    {
+        $tempFile = tempnam(sys_get_temp_dir(), 'test');
+        file_put_contents($tempFile, 'test content');
+        return new Product(
+            Id::generate(),
+            'Образцы документов СИЗ',
+            new Price(450.00, new Currency('RUB')),
+            $tempFile,
+            '1'
+        );
     }
 }
