667f346ca95a424dd4aa86f3f761f280e00ac242
Flaaim <flaaim@list.ru>
2025-10-13 08:34:12 +1000

    added TemplateManager.php

---
+++ src/Shared/Domain/Service/Template/TemplateManager.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Shared\Domain\Service\Template;
+
+use App\Product\Entity\File;
+use App\Shared\Domain\TemplatePath;
+
+
+class TemplateManager
+{
+    private string $templateFile;
+    public function __construct(
+        private readonly TemplatePath $templatePath,
+        private readonly File $file)
+    {
+        $this->templateFile =
+            $this->templatePath->getValue() .
+            DIRECTORY_SEPARATOR .
+            $this->file->getPathToFile();
+    }
+
+    private function templateExists(): bool
+    {
+        return file_exists($this->templateFile);
+    }
+
+    public function getTemplate(): string
+    {
+        if (!$this->templateExists()) {
+            throw new \DomainException('Template files not exists');
+        }
+        return $this->templateFile;
+    }
+
+}
