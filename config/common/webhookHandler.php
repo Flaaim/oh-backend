61601a88428dae39cde3ce26a21e8e7cac97d788
Flaaim <flaaim@list.ru>
2025-10-13 17:10:35 +1000

    refactored WebhookHandler

--- config/common/webhookHandler.php
+++ config/common/webhookHandler.php
@@ -7,6 +7,7 @@ use App\Payment\Service\ProductSender;
 use App\Product\Entity\ProductRepository;
 use App\Shared\Domain\Service\Payment\Provider\YookassaProvider;
 use App\Shared\Domain\Service\Payment\WebhookParser\YookassaWebhookParser;
+use App\Shared\Domain\TemplatePath;
 use Doctrine\ORM\EntityManagerInterface;
 use Psr\Container\ContainerInterface;
 use Symfony\Component\Mailer\MailerInterface;
@@ -16,7 +17,7 @@ return [
         return new HookPaymentHandler(
             new YookassaWebhookParser(),
             $c->get(YookassaProvider::class),
-            new ProductSender($c->get(MailerInterface::class)),
+            new ProductSender($c->get(MailerInterface::class), $c->get(TemplatePath::class)),
             new ProductRepository($c->get(EntityManagerInterface::class)),
             new PaymentRepository($c->get(EntityManagerInterface::class)),
             new Flusher($c->get(EntityManagerInterface::class))
