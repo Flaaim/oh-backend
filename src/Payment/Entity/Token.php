03a80e8898ca4225a57dc910a6ea6dab4cc7cb53
Flaaim <flaaim@list.ru>
2025-10-15 14:47:24 +1000

    added Token mapping

--- src/Payment/Entity/Token.php
+++ src/Payment/Entity/Token.php
@@ -4,10 +4,14 @@ namespace App\Payment\Entity;
 
 use DateTimeImmutable;
 use Webmozart\Assert\Assert;
+use Doctrine\ORM\Mapping as ORM;
 
+#[ORM\Embeddable]
 class Token
 {
+    #[ORM\Column(type:'string', length: 255, nullable: true)]
     private string $value;
+    #[ORM\Column(type: 'datetime_immutable', nullable: true)]
     private DateTimeImmutable $expired;
     public function __construct(string $value, DateTimeImmutable $expired)
     {
