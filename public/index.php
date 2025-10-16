53c192f094c691a2f912aac0a8b6543d9b4900eb
Flaaim <flaaim@list.ru>
2025-10-16 22:33:01 +1000

    one entry point added index

---
+++ public/index.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+use App\Http\Action\CreatePaymentAction;
+use App\Http\JsonResponse;
+use Psr\Container\ContainerInterface;
+use Slim\Psr7\Factory\ServerRequestFactory;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+/** @var  ContainerInterface $container */
+$container = require __DIR__ . '/../config/container.php';
+
+$request = (new ServerRequestFactory())->createFromGlobals();
+
+header('Content-Type: application/json');
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Headers: Content-Type');
+
+switch($request->getUri()->getPath()){
+    case '/process-payment':
+        $action = $container->get(CreatePaymentAction::class);
+        /** @var CreatePaymentAction $action */
+        $response = $action->handle($request);
+        break;
+    default:
+        $response = new JsonResponse(['error' => 'Not found'], 404);
+}
+
+http_response_code($response->getStatusCode());
+foreach ($response->getHeaders() as $name => $values) {
+    foreach ($values as $value) {
+        header(sprintf('%s: %s', $name, $value), false);
+    }
+}
+echo $response->getBody();
