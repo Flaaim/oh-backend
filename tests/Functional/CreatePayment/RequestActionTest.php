65afcd6bcb3d7ad1834581f526bc612a2dd38c66
Flaaim <flaaim@list.ru>
2025-10-17 19:33:35 +1000

    added RequestActionTest.php

---
+++ tests/Functional/CreatePayment/RequestActionTest.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace Test\Functional\CreatePayment;
+
+use DMS\PHPUnitExtensions\ArraySubset\ArraySubsetAsserts;
+use Test\Functional\Json;
+use Test\Functional\WebTestCase;
+use YooKassa\Helpers\UUID;
+
+class RequestActionTest extends WebTestCase
+{
+    use ArraySubsetAsserts;
+    public function testSuccess(): void
+    {
+        $response = $this->handle(self::json('POST', '/process-payment', [
+            'email' => 'test@app.ru',
+            'productId' => 'b38e76c0-ac23-4c48-85fd-975f32c8809f'
+        ]));
+
+        $this->assertEquals(201, $response->getStatusCode());
+        self::assertJson($body = (string)$response->getBody());
+
+        $data = Json::decode($body);
+        self::assertArraySubset([
+            'amount' => 450,
+            'currency' => 'RUB',
+        ],$data);
+    }
+
+    public function testNotFound(): void
+    {
+        $response = $this->handle(self::json('POST', '/process-payment', [
+            'email' => 'test@app.ru',
+            'productId' => \Ramsey\Uuid\Uuid::uuid4()->toString(),
+        ]));
+
+        self::assertEquals(404, $response->getStatusCode());
+        self::assertJson($body = (string)$response->getBody());
+
+        $data = Json::decode($body);
+
+        self::assertArraySubset([
+            'message' => 'Product not found.',
+        ], $data);
+
+    }
+
+    public function testInvalidEmail(): void
+    {
+        $response = $this->handle(self::json('POST', '/process-payment', [
+            'email' => 'invalid',
+            'productId' => 'b38e76c0-ac23-4c48-85fd-975f32c8809f'
+        ]));
+
+        self::assertEquals(400, $response->getStatusCode());
+
+        self::assertJson($body = (string)$response->getBody());
+
+        $data = Json::decode($body);
+
+        self::assertArraySubset([
+            'message' => 'Invalid email address',
+        ], $data);
+    }
+
+    public function testInvalidProductId(): void
+    {
+        $response = $this->handle(self::json('POST', '/process-payment', [
+            'email' => 'test@user.ru',
+            'productId' => 'someInvalidProductId',
+        ]));
+
+        self::assertEquals(400, $response->getStatusCode());
+
+        self::assertJson($body = (string)$response->getBody());
+
+        $data = Json::decode($body);
+
+        self::assertArraySubset([
+            'message' => 'Value "someInvalidProductId" is not a valid UUID.',
+        ], $data);
+    }
+
+}
