a8c9a7bb3cf08e1f12a1222de60152790e080282
Flaaim <flaaim@list.ru>
2025-10-24 12:31:04 +1000

    added NotFoundTest.php

--- tests/Functional/NotFoundTest.php
+++ tests/Functional/NotFoundTest.php
@@ -11,7 +11,7 @@ class NotFoundTest extends WebTestCase
     use ArraySubsetAsserts;
     public function testSuccess(): void
     {
-        $response = $this->handle(self::json('GET', '/'));
+        $response = $this->app()->handle(self::json('GET', '/asda'));
 
         self::assertEquals(404, $response->getStatusCode());
         self::assertJson($body = (string)$response->getBody());
@@ -19,7 +19,7 @@ class NotFoundTest extends WebTestCase
         $data = Json::decode($body);
 
         self::assertArraySubset([
-            'message' => 'Not found',
+            'message' => '404 Not Found',
         ], $data);
     }
 }
