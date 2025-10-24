eb3ad94b5f9f7ff72efcddc473b69bf324145da5
Flaaim <flaaim@list.ru>
2025-10-24 13:05:32 +1000

    remove createPaymentHandler

--- config/common/paymentHandler.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-use App\Payment\Command\CreatePayment\Handler as CreatePaymentHandler;
-
-return [
-    CreatePaymentHandler::class => DI\autowire(CreatePaymentHandler::class),
-];
