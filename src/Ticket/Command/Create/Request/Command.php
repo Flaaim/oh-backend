21e61e19502079905abcc18ab213a12d7e0361be
Flaaim <flaaim@list.ru>
2025-12-06 12:30:17 +1000

    renamed Save to Create

--- src/Ticket/Command/Create/Request/Command.php
+++ src/Ticket/Command/Create/Request/Command.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Ticket\Command\Save\Request;
+namespace App\Ticket\Command\Create\Request;
 
 use Webmozart\Assert\Assert;
 
--- src/Ticket/Command/Create/Request/Handler.php
+++ src/Ticket/Command/Create/Request/Handler.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Ticket\Command\Save\Request;
+namespace App\Ticket\Command\Create\Request;
 
 use App\Flusher;
 use App\Shared\Domain\Response\TicketResponse;
