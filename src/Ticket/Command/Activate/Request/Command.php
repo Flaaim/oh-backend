0ac8004dccd9dfcbc474f50c4dcd3f7d04322bc0
Flaaim <flaaim@list.ru>
2026-01-22 16:39:02 +1000

    changed id

--- src/Ticket/Command/Activate/Request/Command.php
+++ src/Ticket/Command/Activate/Request/Command.php
@@ -5,6 +5,6 @@ namespace App\Ticket\Command\Activate\Request;
 class Command
 {
     public function __construct(
-        public readonly string $ticketId
+        public readonly string $id
     ){}
 }
