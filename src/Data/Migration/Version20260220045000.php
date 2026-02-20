20f14e8791a69711899ca86eb8377f4fbf29d5e3
Flaaim <flaaim@list.ru>
2026-02-20 14:51:35 +1000

    added access migration

---
+++ src/Data/Migration/Version20260220045000.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Data\Migration;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20260220045000 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE accesses (id CHAR(36) NOT NULL, name VARCHAR(255) NOT NULL, cipher VARCHAR(25) NOT NULL, email CHAR(36) NOT NULL, product_id VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE accesses');
+    }
+}
