8bd92ad0a5f2629d20045c48710c409d8450144f
Flaaim <flaaim@list.ru>
2026-02-20 18:00:22 +1000

    added UuidConverter

---
+++ src/Access/Service/UuidConverter.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Access\Service;
+
+use Webmozart\Assert\Assert;
+
+class UuidConverter
+{
+    public function encode(string $uuid): string
+    {
+        Assert::uuid($uuid);
+
+
+        $binaryUuid = hex2bin(str_replace('-', '', $uuid));
+
+        $base64 = base64_encode($binaryUuid);
+
+        return rtrim(strtr($base64, '+/', '-_'), '=');
+    }
+
+    public function decode(string $url): string
+    {
+        $base64 = strtr($url, '-_', '+/');
+
+        $base64 = str_pad($base64, ceil(strlen($base64) / 4) * 4, '=');
+
+        $binary = base64_decode($base64, true);
+
+        if ($binary === false || strlen($binary) !== 16) {
+            throw new \InvalidArgumentException('Invalid token format');
+        }
+
+        $hex = bin2hex($binary);
+
+        return vsprintf('%08s-%04s-%04s-%04s-%012s', [
+            substr($hex, 0, 8),
+            substr($hex, 8, 4),
+            substr($hex, 12, 4),
+            substr($hex, 16, 4),
+            substr($hex, 20, 12)
+        ]);
+    }
+}
---
+++ src/Access/Test/Unit/Service/UuidConverterTest.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Access\Test\Unit\Service;
+
+use App\Access\Service\UuidConverter;
+use PHPUnit\Framework\TestCase;
+use Ramsey\Uuid\Uuid;
+
+class UuidConverterTest extends TestCase
+{
+    public function testConvert(): void
+    {
+        $converter = new UuidConverter();
+        $url = $converter->encode(Uuid::uuid4()->toString());
+
+        self::assertEquals(22, strlen($url));
+        $this->assertMatchesRegularExpression(
+            '/^[a-zA-Z0-9\-_]+$/',
+            $url,
+        );
+    }
+    public function testEmpty(): void
+    {
+        self::expectException(\InvalidArgumentException::class);
+        $converter = new UuidConverter();
+        $converter->encode('');
+    }
+
+    public function testInvalidUuid(): void
+    {
+        self::expectException(\InvalidArgumentException::class);
+        $converter = new UuidConverter();
+        $converter->encode('invalid-uuid');
+    }
+    public function testDecode(): void
+    {
+        $converter = new UuidConverter();
+        $url = $converter->encode($uuid = Uuid::uuid4()->toString());
+
+        $decoded = $converter->decode($url);
+
+        self::assertEquals($uuid, $decoded);
+    }
+    public function testCase(): void
+    {
+        $converter = new UuidConverter();
+        $uuid = strtoupper(Uuid::uuid4()->toString());
+        $url = $converter->encode($uuid);
+
+        $decoded = $converter->decode($url);
+        self::assertEquals(strtolower($uuid), $decoded);
+    }
+
+}
