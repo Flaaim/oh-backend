a872fe417d932c9d5c0a99939e6306d6af95b48e
Flaaim <flaaim@list.ru>
2026-02-20 14:07:06 +1000

    added Test for Product Entity

---
+++ src/Product/Test/Entity/ProductTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Product\Test\Entity;
+
+use App\Product\Entity\Currency;
+use App\Product\Entity\Price;
+use App\Shared\Domain\ValueObject\Id;
+use PHPUnit\Framework\TestCase;
+use Test\Functional\Payment\ProductBuilder;
+
+class ProductTest extends TestCase
+{
+    public function testSuccess(): void
+    {
+        $product = (new ProductBuilder())
+            ->withId($id = new Id('b38e76c0-ac23-4c48-85fd-975f32c8801f'))
+            ->withCipher($cipher = 'ot155.1')
+            ->withPrice($price = new Price(1000.00, new Currency('RUB')))
+            ->withCourse($course = 'ot155')
+            ->build();
+
+        self::assertEquals($id->getValue(), $product->getId()->getValue());
+        self::assertEquals($cipher, $product->getCipher());
+        self::assertEquals($price->getValue(), $product->getPrice()->getValue());
+        self::assertEquals($course, $product->getCourse());
+    }
+}
