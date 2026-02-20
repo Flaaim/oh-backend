35f050c2dd190f422603195b9850b321c98acee7
Flaaim <flaaim@list.ru>
2026-02-20 14:14:12 +1000

    added OpenAccessDTO.php

---
+++ src/Access/Entity/DTO/OpenAccessDTO.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Access\Entity\DTO;
+
+class OpenAccessDTO
+{
+    public function __construct(
+        public string $url,
+        public string $name,
+        public string $cipher
+    ){
+    }
+}
