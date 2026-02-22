f4f473839a8105476f51344b30e15b6ff848a341
Flaaim <flaaim@list.ru>
2026-02-22 13:52:44 +1000

    added FileResponse

---
+++ src/Http/FileResponse.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http;
+
+use Slim\Psr7\Factory\StreamFactory;
+use Slim\Psr7\Headers;
+use Slim\Psr7\Response;
+
+class FileResponse extends Response
+{
+    public function __construct($path, $status = 200)
+    {
+        if (!file_exists($path)) {
+            throw new \InvalidArgumentException(
+                sprintf('File not found: "%s"', $path)
+            );
+        }
+
+        if (!is_file($path)) {
+            throw new \InvalidArgumentException(
+                sprintf('Path is not a file: "%s"', $path)
+            );
+        }
+
+        if (!is_readable($path)) {
+            throw new \InvalidArgumentException(
+                sprintf('File is not readable: "%s"', $path)
+            );
+        }
+        parent::__construct(
+            $status,
+            new Headers([
+                'Content-Type' => 'application/pdf',
+                'Content-Disposition' => 'inline; filename="' . basename($path) . '"',
+                'Content-Length' => filesize($path),
+                'Cache-Control' => 'private, no-cache, no-store, must-revalidate',
+                'Pragma' => 'no-cache',
+                'Expires' => '0',
+                'X-Content-Type-Options' => 'nosniff',
+                'Content-Security-Policy' => 'default-src \'none\'; sandbox',
+            ]),
+            (new StreamFactory())->createStreamFromFile($path)
+        );
+    }
+}
---
+++ src/Http/Test/Unit/FileResponseTest.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Test\Unit;
+
+use App\Http\FileResponse;
+use App\Product\Test\TempDir;
+use PHPUnit\Framework\TestCase;
+
+class FileResponseTest extends TestCase
+{
+    private TempDir $tempDir;
+    protected function setUp(): void
+    {
+        $this->tempDir = TempDir::create();
+    }
+    public function testDefault(): void
+    {
+        $response = new FileResponse($file = $this->tempFile());
+
+        self::assertEquals(200, $response->getStatusCode());
+        self::assertTrue($response->hasHeader('Content-Type'));
+
+        self::assertEquals('application/pdf', $response->getHeaderLine('Content-Type'));
+
+
+        self::assertEquals('inline; filename="' . basename($file) . '"', $response->getHeaderLine('Content-Disposition'));
+
+        self::assertEquals(filesize($file), $response->getHeaderLine('Content-Length'));
+
+        self::assertTrue($response->hasHeader('X-Content-Type-Options'));
+        self::assertEquals('nosniff', $response->getHeaderLine('X-Content-Type-Options'));
+
+        self::assertTrue($response->hasHeader('Content-Security-Policy'));
+        self::assertEquals('default-src \'none\'; sandbox', $response->getHeaderLine('Content-Security-Policy'));
+
+        $body = $response->getBody();
+        self::assertFalse($body->isWritable());
+        self::assertTrue($body->isReadable());
+        self::assertTrue($body->isSeekable());
+
+        $body->rewind();
+        $content = $body->getContents();
+        self::assertEquals('%PDF-1.4 test content', $content);
+    }
+
+    public function testNotFound(): void
+    {
+        self::expectException(\InvalidArgumentException::class);
+        new FileResponse('/file-not-found.pdf');
+
+    }
+    protected function tearDown(): void
+    {
+        $this->tempDir->clear();
+    }
+    private function tempFile(): string
+    {
+        $file = tempnam($this->tempDir->getValue(), 'pdf_test_');
+        $result = file_put_contents($file, '%PDF-1.4 test content');
+        if(!$result) {
+            throw new \RuntimeException('Failed to write temp file');
+        }
+        return $file;
+    }
+}
