e13a11b417504f9e1f3a9815b2acc71e0b192c68
Flaaim <flaaim@list.ru>
2026-03-01 11:45:07 +1000

    added SessionRepository.php

---
+++ src/Access/Entity/SessionRepository.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Access\Entity;
+
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\EntityRepository;
+
+class SessionRepository
+{
+    private EntityRepository $repo;
+    private EntityManagerInterface $em;
+    public function __construct(EntityManagerInterface $em)
+    {
+        $repo = $em->getRepository(Session::class);
+        $this->repo = $repo;
+        $this->em = $em;
+    }
+    public function findByToken(string $token): ?Session
+    {
+        return $this->repo->findOneBy(['token' => $token]);
+    }
+
+    public function create(Session $session): void
+    {
+        $this->em->persist($session);
+    }
+}
