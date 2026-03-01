86a62fae24f786b7744a9f9230d28f139ebcc0f4
Flaaim <flaaim@list.ru>
2026-03-01 13:14:28 +1000

    added access_sessions migrations

---
+++ src/Data/Migration/Version20260301015741.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Data\Migration;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20260301015741 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE access_sessions (token VARCHAR(255) NOT NULL, session_id VARCHAR(64) NOT NULL, ip VARCHAR(64) NOT NULL, user_agent VARCHAR(45) NOT NULL, last_seen DATETIME NOT NULL, created_at DATETIME NOT NULL, PRIMARY KEY(token)) DEFAULT CHARACTER SET utf8mb4');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE access_sessions');
+    }
+}
