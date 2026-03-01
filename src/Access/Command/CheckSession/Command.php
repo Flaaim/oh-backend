a117996c606d5371cc6b25e302e4845c97cd5bff
Flaaim <flaaim@list.ru>
2026-03-01 11:45:42 +1000

    added CheckSession handler

---
+++ src/Access/Command/CheckSession/Command.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Access\Command\CheckSession;
+
+use Symfony\Component\Validator\Constraints as Assert;
+
+class Command
+{
+    public function __construct(
+        #[Assert\NotBlank]
+        #[Assert\Length(exactly: 22)]
+        #[Assert\Regex(
+            pattern: '/^[A-Za-z0-9_-]+$/',
+            message: 'The format URL must be a valid.',
+        )]
+        public string $token,
+        #[Assert\NotBlank]
+        public string $sessionId,
+        #[Assert\NotBlank]
+        public string $ip,
+        #[Assert\NotBlank]
+        public string $userAgent,
+    )
+    {}
+}
---
+++ src/Access/Command/CheckSession/Handler.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Access\Command\CheckSession;
+
+use App\Access\Entity\Session;
+use App\Access\Entity\SessionRepository;
+use App\Access\Service\UuidConverter;
+use App\Flusher;
+
+class Handler
+{
+
+    public function __construct(
+        private readonly SessionRepository $sessions,
+        private readonly UuidConverter $uuidConverter,
+        private readonly Flusher $flusher
+    )
+    {}
+
+    public function handle(Command $command): void
+    {
+        $decodedToken = $this->uuidConverter->decode($command->token);
+
+        $session = $this->sessions->findByToken($decodedToken);
+
+        if (!$session) {
+            $this->sessions->create(
+                new Session(
+                    $decodedToken,
+                    $command->sessionId,
+                    $command->ip,
+                    $command->userAgent,
+                )
+            );
+            $this->flusher->flush();
+            return;
+        }
+
+        if($session->isExpired(30)){
+            $session->replace(
+                $command->sessionId,
+                $command->ip,
+                $command->userAgent,
+            );
+        }else{
+            $session->assertSameSession($command->sessionId);
+            $session->touch();
+        }
+
+        $this->flusher->flush();
+
+    }
+
+}
