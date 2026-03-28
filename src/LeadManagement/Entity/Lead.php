290fb9d0eab54e95dc017446952352a35054c1e8
Flaaim <flaaim@list.ru>
2026-03-28 19:35:27 +1000

    added Lead Entity

---
+++ src/LeadManagement/Entity/Lead.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\LeadManagement\Entity;
+
+class Lead
+{
+    public function __construct(
+        private LeadId $leadId,
+        private string $name,
+        private string $contact,
+        private string $message,
+    ){
+    }
+}
