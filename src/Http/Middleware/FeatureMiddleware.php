c43e0ed6d02be295271db8270910bbcd7a232eab
Flaaim <flaaim@list.ru>
2026-05-05 13:14:13 +1000

    added FeatureMiddleware.php

---
+++ src/Http/Middleware/FeatureMiddleware.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\FeatureToggle\FeatureSwitch;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+
+class FeatureMiddleware implements MiddlewareInterface
+{
+    public function __construct(
+        private readonly FeatureSwitch $features,
+        private readonly string        $header = 'X-Features'
+    ) {
+    }
+    #[\Override]
+    public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
+    {
+        $header = $request->getHeaderLine($this->header);
+        $features = array_filter(preg_split('/\s*,\s*/', $header));
+
+        foreach ($features as $feature) {
+            $this->features->enable($feature);
+        }
+        return $handler->handle($request);
+    }
+}
